294a80159ed9912ff7fd82e077a1813bb3b14cbb
Calvin Vriends <93667053+CalvinVriendsNOV@users.noreply.github.com>
2024-01-11 12:49:37 +0000

    Quizes

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+
+const PRICE_PER_APPLE: i32 = 2;
+const PRICE_PER_APPLE_ABOVE_40: i32 = 1;
+
+fn calculate_price_of_apples(amount: i32) -> i32 {
+    amount * if amount > 40 {PRICE_PER_APPLE_ABOVE_40} else {PRICE_PER_APPLE}
+}
 
 // Don't modify this function!
 #[test]
